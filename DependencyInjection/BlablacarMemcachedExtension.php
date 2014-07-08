aef60e50d54e25108ead79e890e1fb83321072a3
Olivier Dolbeau <contact@odolbeau.fr>
2014-07-08 11:26:10 +0200

    Add missing use

--- DependencyInjection/BlablacarMemcachedExtension.php
+++ DependencyInjection/BlablacarMemcachedExtension.php
@@ -7,6 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class BlablacarMemcachedExtension extends Extension
 {
